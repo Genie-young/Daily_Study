422eecd031c2e8384f26ce43ef83751c9ca0baae
genie <siler2312@gmail.com>
2019-06-18 10:01:35 +0900

    easyDP(2019-06-18)

---
+++ Algorithms/EasyDP_Backjoon11726.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class EasyDP_Backjoon11726 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		if(n <=2){
+			System.out.println(n);
+			return;
+		}
+		int dp[] = new int[n + 1];
+		dp[1] =1;
+		dp[2] =2;
+		for (int i = 3; i <= n; i++) {
+			dp[i] = (dp[i-1]+dp[i-2])%10007;
+		}
+		System.out.println(dp[n]);
+	}
+}
